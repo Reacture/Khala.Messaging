931f3c4bcc6af282c72459802be725c4f0064e8a
Gyuwon Yi <gyuwon@live.com>
2017-01-16 18:54:11 +0900

    IMessageBus 인터페이스를 추가합니다.

---
+++ source/RA.Messaging.Core/Messaging/IMessageBus.cs
@@ -0,0 +1,17 @@
+﻿namespace ReactiveArchitecture.Messaging
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public interface IMessageBus
+    {
+        Task Send(
+            object message,
+            CancellationToken cancellationToken);
+
+        Task SendBatch(
+            IEnumerable<object> messages,
+            CancellationToken cancellationToken);
+    }
+}
