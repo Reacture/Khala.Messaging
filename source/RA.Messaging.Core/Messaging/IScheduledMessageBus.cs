3af43eca04e5bc063d1beb31cacf26a6a42d048e
Gyuwon Yi <gyuwon@live.com>
2017-01-16 19:01:21 +0900

    IScheduledMessageBus 인터페이스를 추가합니다.

---
+++ source/RA.Messaging.Core/Messaging/IScheduledMessageBus.cs
@@ -0,0 +1,14 @@
+﻿namespace ReactiveArchitecture.Messaging
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public interface IScheduledMessageBus
+    {
+        Task Send(
+            object message,
+            DateTimeOffset scheduledTimeUtc,
+            CancellationToken cancellationToken);
+    }
+}
