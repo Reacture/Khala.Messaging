70f715079c031c517e20fcc8c869c64a0b226944
Gyuwon Yi <gyuwon@live.com>
2017-01-18 00:33:40 +0900

    CompositeMessageHandler 클래스를 추가합니다.

---
+++ source/RA.Messaging.Core/Messaging/CompositeMessageHandler.cs
@@ -0,0 +1,68 @@
+﻿namespace ReactiveArchitecture.Messaging
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class CompositeMessageHandler : IMessageHandler
+    {
+        private readonly IEnumerable<IMessageHandler> _handlers;
+
+        public CompositeMessageHandler(params IMessageHandler[] handlers)
+        {
+            if (handlers == null)
+            {
+                throw new ArgumentNullException(nameof(handlers));
+            }
+
+            var handlerList = new List<IMessageHandler>(handlers);
+            for (int i = 0; i < handlerList.Count; i++)
+            {
+                if (handlerList[i] == null)
+                {
+                    throw new ArgumentException(
+                        $"{nameof(handlers)} cannot contain null.",
+                        nameof(handlers));
+                }
+            }
+
+            _handlers = new ReadOnlyCollection<IMessageHandler>(handlerList);
+        }
+
+        async Task IMessageHandler.Handle(
+            object message,
+            CancellationToken cancellationToken)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+
+            List<Exception> exceptions = null;
+
+            foreach (IMessageHandler handler in _handlers)
+            {
+                try
+                {
+                    await handler.Handle(message, cancellationToken);
+                }
+                catch (Exception exception)
+                {
+                    if (exceptions == null)
+                    {
+                        exceptions = new List<Exception>();
+                    }
+
+                    exceptions.Add(exception);
+                }
+            }
+
+            if (exceptions != null)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+    }
+}
---
+++ source/RA.Messaging.Tests/Messaging/CompositeMessageHandler_features.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+using Ploeh.AutoFixture.Idioms;
+using Xunit;
+
+namespace ReactiveArchitecture.Messaging
+{
+    public class CompositeMessageHandler_features
+    {
+        [Fact]
+        public void sut_implements_IMessageHandler()
+        {
+            var sut = new CompositeMessageHandler();
+            sut.Should().BeAssignableTo<IMessageHandler>();
+        }
+
+        [Fact]
+        public void class_has_guard_clauses()
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            var assertion = new GuardClauseAssertion(fixture);
+            assertion.Verify(typeof(CompositeMessageHandler));
+        }
+
+        [Fact]
+        public void constructor_has_guard_clause_for_null_handler()
+        {
+            Action action = () => new CompositeMessageHandler(
+                Mock.Of<IMessageHandler>(),
+                default(IMessageHandler),
+                Mock.Of<IMessageHandler>());
+
+            action.ShouldThrow<ArgumentException>()
+                .Where(x => x.ParamName == "handlers");
+        }
+
+        [Fact]
+        public async Task Handle_sends_message_to_all_handlers()
+        {
+            var handler1 = Mock.Of<IMessageHandler>();
+            var handler2 = Mock.Of<IMessageHandler>();
+            IMessageHandler sut =
+                new CompositeMessageHandler(handler1, handler2);
+            var message = new object();
+
+            await sut.Handle(message, CancellationToken.None);
+
+            Mock.Get(handler1).Verify(
+                x => x.Handle(message, CancellationToken.None), Times.Once());
+            Mock.Get(handler2).Verify(
+                x => x.Handle(message, CancellationToken.None), Times.Once());
+        }
+
+        [Fact]
+        public async Task Handle_sends_message_to_all_handlers_even_if_some_handler_fails()
+        {
+            var handler1 = Mock.Of<IMessageHandler>();
+            var handler2 = Mock.Of<IMessageHandler>();
+            IMessageHandler sut =
+                new CompositeMessageHandler(handler1, handler2);
+            var message = new object();
+            Mock.Get(handler1)
+                .Setup(x => x.Handle(message, CancellationToken.None))
+                .Throws<InvalidOperationException>();
+
+            try
+            {
+                await sut.Handle(message, CancellationToken.None);
+            }
+            catch
+            {
+            }
+
+            Mock.Get(handler1).Verify(
+                x => x.Handle(message, CancellationToken.None), Times.Once());
+            Mock.Get(handler2).Verify(
+                x => x.Handle(message, CancellationToken.None), Times.Once());
+        }
+
+        [Fact]
+        public void Handle_throws_aggregate_exception_if_handler_fails()
+        {
+            // Arrange
+            var handler1 = Mock.Of<IMessageHandler>();
+            var handler2 = Mock.Of<IMessageHandler>();
+
+            IMessageHandler sut =
+                new CompositeMessageHandler(handler1, handler2);
+
+            var message = new object();
+
+            var exception1 = new InvalidOperationException();
+            Mock.Get(handler1)
+                .Setup(x => x.Handle(message, CancellationToken.None))
+                .Throws(exception1);
+
+            var exception2 = new InvalidOperationException();
+            Mock.Get(handler2)
+                .Setup(x => x.Handle(message, CancellationToken.None))
+                .Throws(exception2);
+
+            // Act
+            Func<Task> action = () => sut.Handle(message, CancellationToken.None);
+
+            // Arrange
+            action.ShouldThrow<AggregateException>()
+                .Which.InnerExceptions.Should().ContainSingle()
+                .Which.Should().BeOfType<AggregateException>()
+                .Which.InnerExceptions
+                .ShouldAllBeEquivalentTo(new[] { exception1, exception2 });
+        }
+    }
+}
