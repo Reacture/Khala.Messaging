9d0e8071d182857aa1be495d01faaf521e4f0517
Gyuwon Yi <gyuwon@live.com>
2017-01-17 22:10:10 +0900

    IMessageHandler 인터페이스를 추가합니다.

---
+++ source/RA.Messaging.Core/Messaging/IMessageHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace ReactiveArchitecture.Messaging
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public interface IMessageHandler
+    {
+        Task Handle(object message, CancellationToken cancellationToken);
+    }
+}
