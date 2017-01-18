3f0902db62f9ccccffa312b2f662261dd4016318
Gyuwon Yi <gyuwon@live.com>
2017-01-19 02:15:07 +0900

    EventMessageProcessorFactory 클래스를 추가합니다.

---
+++ source/RA.Messaging.Azure/Messaging/Azure/EventMessageProcessorFactory.cs
@@ -0,0 +1,32 @@
+﻿namespace ReactiveArchitecture.Messaging.Azure
+{
+    using System;
+    using Microsoft.ServiceBus.Messaging;
+
+    public class EventMessageProcessorFactory : IEventProcessorFactory
+    {
+        private readonly IMessageHandler _handler;
+        private readonly IMessageSerializer _serializer;
+
+        public EventMessageProcessorFactory(
+            IMessageHandler handler,
+            IMessageSerializer serializer)
+        {
+            if (handler == null)
+            {
+                throw new ArgumentNullException(nameof(handler));
+            }
+
+            if (serializer == null)
+            {
+                throw new ArgumentNullException(nameof(serializer));
+            }
+
+            _handler = handler;
+            _serializer = serializer;
+        }
+
+        public IEventProcessor CreateEventProcessor(PartitionContext context)
+            => new EventMessageProcessor(_handler, _serializer);
+    }
+}
