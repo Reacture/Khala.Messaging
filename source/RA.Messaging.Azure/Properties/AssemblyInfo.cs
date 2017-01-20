4a7148fc46b61d868fcce247f7b2b42bb0ad1b9a
Gyuwon Yi <gyuwon@live.com>
2017-01-20 12:18:56 +0900

    v0.1.4

--- source/RA.Messaging.Azure/Properties/AssemblyInfo.cs
+++ source/RA.Messaging.Azure/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright Gyuwon 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: Guid("2a5f396f-c3e1-4c37-bb6b-4ed2bf207de6")]
-[assembly: AssemblyVersion("0.1.3.0")]
-[assembly: AssemblyFileVersion("0.1.3.0")]
+[assembly: AssemblyVersion("0.1.4.0")]
+[assembly: AssemblyFileVersion("0.1.4.0")]
