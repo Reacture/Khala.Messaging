4f13f7e2ff1a8a114091f73f7d7c7c3e375a10bb
Gyuwon Yi <gyuwon@live.com>
2017-01-22 14:58:29 +0900

    v0.1.5

--- source/RA.Messaging.Azure.Owin/Properties/AssemblyInfo.cs
+++ source/RA.Messaging.Azure.Owin/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright Gyuwon 2017")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9b741cd8-b9db-467b-a630-05d9d6cfbf2d")]
-[assembly: AssemblyVersion("0.1.4.0")]
-[assembly: AssemblyFileVersion("0.1.4.0")]
+[assembly: AssemblyVersion("0.1.5.0")]
+[assembly: AssemblyFileVersion("0.1.5.0")]
