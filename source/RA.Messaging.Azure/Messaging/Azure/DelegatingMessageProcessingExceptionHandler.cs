f8ecf626b48834f5a4aa11baf80572c299e11230
Gyuwon Yi <gyuwon@live.com>
2017-01-27 02:22:16 +0900

    DelegatingMessageProcessingExceptionHandler<TSource> 클래스를 추가합니다.

---
+++ source/RA.Messaging.Azure/Messaging/Azure/DelegatingMessageProcessingExceptionHandler.cs
@@ -0,0 +1,47 @@
+﻿namespace ReactiveArchitecture.Messaging.Azure
+{
+    using System;
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+
+    public class DelegatingMessageProcessingExceptionHandler<TSource> :
+        IMessageProcessingExceptionHandler<TSource>
+        where TSource : class
+    {
+        private readonly Func<MessageProcessingExceptionContext<TSource>, Task> _handler;
+
+        public DelegatingMessageProcessingExceptionHandler(
+            Func<MessageProcessingExceptionContext<TSource>, Task> handler)
+        {
+            if (handler == null)
+            {
+                throw new ArgumentNullException(nameof(handler));
+            }
+
+            _handler = handler;
+        }
+
+        public Task Handle(MessageProcessingExceptionContext<TSource> context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            return InvokeHandler(context);
+        }
+
+        private async Task InvokeHandler(
+            MessageProcessingExceptionContext<TSource> context)
+        {
+            try
+            {
+                await _handler.Invoke(context);
+            }
+            catch (Exception handlerError)
+            {
+                Trace.TraceError(handlerError.ToString());
+            }
+        }
+    }
+}
