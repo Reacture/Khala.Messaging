46b08a814d3c8265e437b64c6aa707a41bf6df15
Gyuwon Yi <gyuwon@live.com>
2017-01-26 15:27:03 +0900

    IMessageProcessingExceptionHandler<TSource> 인터페이스를 추가합니다.

---
+++ source/RA.Messaging.Azure/Messaging/Azure/IMessageProcessingExceptionHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace ReactiveArchitecture.Messaging.Azure
+{
+    using System.Threading.Tasks;
+
+    public interface IMessageProcessingExceptionHandler<TSource>
+        where TSource : class
+    {
+        Task Handle(MessageProcessingExceptionContext<TSource> context);
+    }
+}
