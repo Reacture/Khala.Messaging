7ba092ba91d2b31789c0b583701e54adb372ade6
Gyuwon Yi <gyuwon@live.com>
2017-01-30 11:55:14 +0900

    MessagingExtensions 클래스를 추가합니다.

---
+++ source/Arcane.Messaging.Tests/Messaging/MessagingExtensions_features.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace Arcane.Messaging
+{
+    public class MessagingExtensions_features
+    {
+        [Fact]
+        public void Send_relays_with_none_cancellation_token()
+        {
+            var task = Task.FromResult(true);
+            var envelope = new Envelope(new object());
+            var messageBus = Mock.Of<IMessageBus>(
+                x => x.Send(envelope, CancellationToken.None) == task);
+
+            Task result = messageBus.Send(envelope);
+
+            Mock.Get(messageBus).Verify(
+                x => x.Send(envelope, CancellationToken.None), Times.Once());
+            result.Should().BeSameAs(task);
+        }
+
+        [Fact]
+        public void SendBatch_relays_with_none_cancellation_token()
+        {
+            var task = Task.FromResult(true);
+            var envelopes = new Envelope[] { };
+            var messageBus = Mock.Of<IMessageBus>(
+                x => x.SendBatch(envelopes, CancellationToken.None) == task);
+
+            Task result = messageBus.SendBatch(envelopes);
+
+            Mock.Get(messageBus).Verify(
+                x => x.SendBatch(envelopes, CancellationToken.None), Times.Once());
+            result.Should().BeSameAs(task);
+        }
+    }
+}
---
+++ source/Arcane.Messaging/Messaging/MessagingExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace Arcane.Messaging
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public static class MessagingExtensions
+    {
+        public static Task Send(this IMessageBus messageBus, Envelope envelope)
+        {
+            if (messageBus == null)
+            {
+                throw new ArgumentNullException(nameof(messageBus));
+            }
+
+            return messageBus.Send(envelope, CancellationToken.None);
+        }
+
+        public static Task SendBatch(
+            this IMessageBus messageBus,
+            IEnumerable<Envelope> envelopes)
+        {
+            if (messageBus == null)
+            {
+                throw new ArgumentNullException(nameof(messageBus));
+            }
+
+            return messageBus.SendBatch(envelopes, CancellationToken.None);
+        }
+    }
+}
