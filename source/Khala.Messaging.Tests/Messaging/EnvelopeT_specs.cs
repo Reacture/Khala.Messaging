2e921ddf539750c6be94ebe1b09cb26317ed5f86
Gyuwon <gyuwon@live.com>
2017-09-06 12:12:04 +0900

    Envelope<T> 클래스 테스트 케이스를 보강합니다.

---
+++ source/Khala.Messaging.Tests/Messaging/EnvelopeT_specs.cs
@@ -0,0 +1,38 @@
+﻿namespace Khala.Messaging
+{
+    using System;
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Ploeh.AutoFixture;
+    using Ploeh.AutoFixture.Idioms;
+
+    [TestClass]
+    public class EnvelopeT_specs
+    {
+        [TestMethod]
+        public void sut_has_guard_clauses()
+        {
+            new GuardClauseAssertion(new Fixture()).Verify(typeof(Envelope<>));
+        }
+
+        [TestMethod]
+        public void constructor_has_guard_clause_against_empty_correlation_id()
+        {
+            Action action = () =>
+            new Envelope<Message>(Guid.NewGuid(), correlationId: Guid.Empty, message: new Message());
+            action.ShouldThrow<ArgumentException>().Where(x => x.ParamName == "correlationId");
+        }
+
+        [TestMethod]
+        public void constructor_has_guard_clause_against_null_message()
+        {
+            Action action = () =>
+            new Envelope<Message>(Guid.NewGuid(), null, message: null);
+            action.ShouldThrow<ArgumentException>().Where(x => x.ParamName == "message");
+        }
+
+        public class Message
+        {
+        }
+    }
+}
