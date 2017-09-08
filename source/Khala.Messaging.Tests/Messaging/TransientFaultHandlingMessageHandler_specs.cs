4c8ff065750d4087694d73e33e772fb68b3c85f6
Gyuwon <gyuwon@live.com>
2017-09-08 16:14:24 +0900

    TransientFaultHandlingMessageHandler 클래스를 추가합니다.

---
+++ source/Khala.Messaging.Tests/Messaging/TransientFaultHandlingMessageHandler_specs.cs
@@ -0,0 +1,52 @@
+﻿namespace Khala.Messaging
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using Khala.TransientFaultHandling;
+    using Khala.TransientFaultHandling.Testing;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using Ploeh.AutoFixture;
+    using Ploeh.AutoFixture.AutoMoq;
+    using Ploeh.AutoFixture.Idioms;
+
+    [TestClass]
+    public class TransientFaultHandlingMessageHandler_specs
+    {
+        [TestMethod]
+        public void sut_implements_IMessageHandler()
+        {
+            typeof(TransientFaultHandlingMessageHandler).Should().Implement<IMessageHandler>();
+        }
+
+        [TestMethod]
+        public void sut_has_guard_clauses()
+        {
+            var builder = new Fixture().Customize(new AutoMoqCustomization());
+            new GuardClauseAssertion(builder).Verify(typeof(TransientFaultHandlingMessageHandler));
+        }
+
+        [TestMethod]
+        public async Task Handle_relays_with_retry_policy()
+        {
+            // Arrange
+            var spy = new TransientFaultHandlingSpy();
+            RetryPolicy retryPolicy = spy.Policy;
+            var envelope = new Envelope(new object());
+            var messageHandler = Mock.Of<IMessageHandler>();
+            Mock.Get(messageHandler)
+                .Setup(x => x.Handle(envelope, CancellationToken.None))
+                .Callback<Envelope, CancellationToken>((envelopeParam, cancellationToken) => spy.OperationCancellable.Invoke(cancellationToken))
+                .Returns(Task.FromResult(true));
+            var sut = new TransientFaultHandlingMessageHandler(retryPolicy, messageHandler);
+
+            // Act
+            await sut.Handle(envelope);
+
+            // Assert
+            spy.Verify();
+            Mock.Get(messageHandler).Verify(x => x.Handle(envelope, CancellationToken.None));
+        }
+    }
+}
---
+++ source/Khala.Messaging/Messaging/TransientFaultHandlingMessageHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace Khala.Messaging
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Khala.TransientFaultHandling;
+
+    public class TransientFaultHandlingMessageHandler : IMessageHandler
+    {
+        private RetryPolicy _retryPolicy;
+        private IMessageHandler _messageHandler;
+
+        public TransientFaultHandlingMessageHandler(RetryPolicy retryPolicy, IMessageHandler messageHandler)
+        {
+            _retryPolicy = retryPolicy ?? throw new ArgumentNullException(nameof(retryPolicy));
+            _messageHandler = messageHandler ?? throw new ArgumentNullException(nameof(messageHandler));
+        }
+
+        public Task Handle(Envelope envelope, CancellationToken cancellationToken)
+        {
+            if (envelope == null)
+            {
+                throw new ArgumentNullException(nameof(envelope));
+            }
+
+            return _retryPolicy.Run(() => _messageHandler.Handle(envelope, cancellationToken));
+        }
+    }
+}
