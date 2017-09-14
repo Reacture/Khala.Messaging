d4d3dfcd019165c90fdf20d4bbb5a62276cbcebc
Gyuwon <gyuwon@live.com>
2017-09-14 22:12:12 +0900

    TransientFaultHandlingMessageBus 클래스를 추가합니다.

---
+++ source/Khala.Messaging.Tests/Messaging/TransientFaultHandlingMessageBus_specs.cs
@@ -0,0 +1,157 @@
+﻿namespace Khala.Messaging
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using Khala.TransientFaultHandling;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using Ploeh.AutoFixture;
+    using Ploeh.AutoFixture.AutoMoq;
+    using Ploeh.AutoFixture.Idioms;
+
+    [TestClass]
+    public class TransientFaultHandlingMessageBus_specs
+    {
+        [TestMethod]
+        public void sut_implements_IMessageBus()
+        {
+            typeof(TransientFaultHandlingMessageBus).Should().Implement<IMessageBus>();
+        }
+
+        [TestMethod]
+        public void constructor_sets_RetryPolicy_correctly()
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            var retryPolicy = fixture.Create<RetryPolicy>();
+            var messageBus = Mock.Of<IMessageBus>();
+
+            var sut = new TransientFaultHandlingMessageBus(retryPolicy, messageBus);
+
+            sut.RetryPolicy.Should().BeSameAs(retryPolicy);
+        }
+
+        [TestMethod]
+        public void sut_is_immutable()
+        {
+            foreach (PropertyInfo property in typeof(TransientFaultHandlingMessageBus).GetProperties())
+            {
+                property.Should().NotBeWritable();
+            }
+        }
+
+        [TestMethod]
+        public void sut_has_RetryPolicy_property()
+        {
+            typeof(TransientFaultHandlingMessageBus).Should().HaveProperty<RetryPolicy>("RetryPolicy");
+        }
+
+        [TestMethod]
+        public void sut_has_MessageBus_property()
+        {
+            typeof(TransientFaultHandlingMessageBus).Should().HaveProperty<IMessageBus>("MessageBus");
+        }
+
+        [TestMethod]
+        public void constructor_sets_MessageBus_correctly()
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            var retryPolicy = fixture.Create<RetryPolicy>();
+            var messageBus = Mock.Of<IMessageBus>();
+
+            var sut = new TransientFaultHandlingMessageBus(retryPolicy, messageBus);
+
+            sut.MessageBus.Should().BeSameAs(messageBus);
+        }
+
+        [TestMethod]
+        public void sut_has_guard_clauses()
+        {
+            var builder = new Fixture().Customize(new AutoMoqCustomization());
+            new GuardClauseAssertion(builder).Verify(typeof(TransientFaultHandlingMessageBus));
+        }
+
+        [TestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public async Task Send_relays_to_retry_policy(bool canceled)
+        {
+            // Arrange
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+            var maximumRetryCount = 1;
+            var retryPolicyMock = new Mock<RetryPolicy>(
+                maximumRetryCount,
+                new TransientFaultDetectionStrategy(),
+                fixture.Create<RetryIntervalStrategy>());
+
+            var messageBus = Mock.Of<IMessageBus>();
+
+            var sut = new TransientFaultHandlingMessageBus(
+                retryPolicyMock.Object,
+                messageBus);
+
+            var envelope = new Envelope(new object());
+            var cancellationToken = new CancellationToken(canceled);
+
+            // Act
+            await sut.Send(envelope, cancellationToken);
+
+            // Assert
+            Func<Envelope, CancellationToken, Task> func = messageBus.Send;
+            retryPolicyMock.Verify(
+                x =>
+                x.Run(
+                    It.Is<Func<Envelope, CancellationToken, Task>>(
+                        p =>
+                        p.Method == func.Method &&
+                        p.Target == func.Target),
+                    envelope,
+                    cancellationToken),
+                Times.Once());
+        }
+
+        [TestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public async Task SendBatch_relays_to_retry_policy(bool canceled)
+        {
+            // Arrange
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+            var maximumRetryCount = 1;
+            var retryPolicyMock = new Mock<RetryPolicy>(
+                maximumRetryCount,
+                new TransientFaultDetectionStrategy(),
+                fixture.Create<RetryIntervalStrategy>());
+
+            var messageBus = Mock.Of<IMessageBus>();
+
+            var sut = new TransientFaultHandlingMessageBus(
+                retryPolicyMock.Object,
+                messageBus);
+
+            var envelopes = new[] { new Envelope(new object()) };
+            var cancellationToken = new CancellationToken(canceled);
+
+            // Act
+            await sut.SendBatch(envelopes, cancellationToken);
+
+            // Assert
+            Func<IEnumerable<Envelope>, CancellationToken, Task> func = messageBus.SendBatch;
+            retryPolicyMock.Verify(
+                x =>
+                x.Run(
+                    It.Is<Func<IEnumerable<Envelope>, CancellationToken, Task>>(
+                        p =>
+                        p.Method == func.Method &&
+                        p.Target == func.Target),
+                    envelopes,
+                    cancellationToken),
+                Times.Once());
+        }
+    }
+}
---
+++ source/Khala.Messaging/Messaging/TransientFaultHandlingMessageBus.cs
@@ -0,0 +1,41 @@
+﻿namespace Khala.Messaging
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Khala.TransientFaultHandling;
+
+    public class TransientFaultHandlingMessageBus : IMessageBus
+    {
+        public TransientFaultHandlingMessageBus(RetryPolicy retryPolicy, IMessageBus messageBus)
+        {
+            RetryPolicy = retryPolicy ?? throw new ArgumentNullException(nameof(retryPolicy));
+            MessageBus = messageBus ?? throw new ArgumentNullException(nameof(messageBus));
+        }
+
+        public RetryPolicy RetryPolicy { get; }
+
+        public IMessageBus MessageBus { get; }
+
+        public Task Send(Envelope envelope, CancellationToken cancellationToken)
+        {
+            if (envelope == null)
+            {
+                throw new ArgumentNullException(nameof(envelope));
+            }
+
+            return RetryPolicy.Run(MessageBus.Send, envelope, cancellationToken);
+        }
+
+        public Task SendBatch(IEnumerable<Envelope> envelopes, CancellationToken cancellationToken)
+        {
+            if (envelopes == null)
+            {
+                throw new ArgumentNullException(nameof(envelopes));
+            }
+
+            return RetryPolicy.Run(MessageBus.SendBatch, envelopes, cancellationToken);
+        }
+    }
+}
