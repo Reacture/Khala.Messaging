463ff7b3bde11d9da260d83c5976ef2b5dbeaec3
Gyuwon Yi <gyuwon@live.com>
2018-03-18 18:14:56 +0900

    .NET Framework 4.7 종속 테스트 분리
    테스트 프로젝트를 .NET Core로 전환하기 전에 .NET Framework 4.7에 종속된
    테스트를 별도의 프로젝트로 분리한다.

---
+++ source/Khala.Messaging.Net47.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Khala.Messaging.Net47.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Khala.Messaging.Net47.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5540d19a-a867-4c07-9a3c-3131c0c1af9b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
